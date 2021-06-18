720fe6a2635a2378309d38cb3884d4b6edcaba3e
CatFishtech <80201866+CatFishtech@users.noreply.github.com>
2021-06-18 19:07:11 +0300

    update

--- register_a.php
+++ register_a.php
@@ -18,7 +18,7 @@ else(isset($_POST['save']))
     {
         $query="INSERT INTO register(First_Name, Last_Name, Email, Password, File ) VALUES ('$first_name', '$last_name', '$email', 'md5($pass)', '$final_file')";
         $sql=mysqli_query($conn,$query)or die("Could Not Perform the Query");
-        header ("Location: login.php?status=success");
+        header ("Location: login.php? status=success");
     }
     else 
     {
