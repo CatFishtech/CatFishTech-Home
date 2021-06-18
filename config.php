b121e19f6d44326e07060e5054db16863b3c580c
CatFishtech <80201866+CatFishtech@users.noreply.github.com>
2021-06-18 19:11:38 +0300

    update

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 <?php
 // Enter your Host, username, password, database below.
 // I left password empty because i do not set password on localhost.
-$con = mysqli_connect("remotemysql.com","SrTfMJpCxK","taBr4zjoLP","SrTfMJpCxK");
+$con = mysqli_connect("localhost","SrTfMJpCxK","taBr4zjoLP","SrTfMJpCxK");
 // Check connection
 if (mysqli_connect_errno())
   {
