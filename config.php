e955c632e2ff05ecbd887a0f1cb074197b616ecd
CatFishtech <80201866+CatFishtech@users.noreply.github.com>
2021-06-18 18:41:04 +0300

    update

--- config.php
+++ config.php
@@ -2,7 +2,8 @@
     $url='remotemysql.com';
     $username='SrTfMJpCxK';
     $password='taBr4zjoLP';
-    $conn=mysqli_connect($url,$username,$password,"fregister");
+    $dbname='SrTfMJpCxK'
+    $conn=mysqli_connect($url,$username,$password,$dbname);
     if(!$conn){
         die('Could not Connect My Sql:' .mysql_error());
     }
