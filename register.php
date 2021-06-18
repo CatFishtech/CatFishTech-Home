886f358af97f97b52cd4a8f15873c239ff21e12a
CatFishtech <80201866+CatFishtech@users.noreply.github.com>
2021-06-18 17:35:00 +0300

    update

--- register.php
+++ register.php
@@ -31,8 +31,7 @@
 
       <div class="signup-form">
     <form action="register_a.php" method="post" enctype="multipart/form-data">
-		<h2>Register</h2>
-		<p class="hint-text">Create your account</p>
+		<h2 class="hint-text" align="center">Create your account</h2>
         <div class="form-group">
 			<div class="row">
 				<div class="col"><input type="text" class="form-control" name="first_name" placeholder="First Name" required="required"></div>
@@ -48,10 +47,7 @@
 		<div class="form-group">
             <input type="password" class="form-control" name="cpass" placeholder="Confirm Password" required="required">
         </div>
-        <div class="form-group">
-            <input type="file" name="file" required>
-            <!-- <input type="submit" name="upload" value="Upload" class="btn"> -->
-        </div>        
+                
         <div class="form-group">
 			<label class="form-check-label"><input type="checkbox" required="required"> I accept the <a href="#">Terms of Use</a> & <a href="#">Privacy Policy</a></label>
 		</div>
