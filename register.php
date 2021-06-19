857bf039e6ad4f32890ce5770d0d377fad87face
CatFishtech <80201866+CatFishtech@users.noreply.github.com>
2021-06-19 13:28:41 +0300

    update

--- register.php
+++ register.php
@@ -166,17 +166,25 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" enctype="multipart/form-data">
 		<h2 class="hint-text" align="center">Create your account</h2>
 				<div class="form-group">
-        <input type="fullname" class="form-control" name="fullname" placeholder="Full Name" required="required">
+        <input type="fullname" class="form-control" name="fullname" placeholder="Full Name"<?php echo (!empty($fullname_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $fullname; ?>">
+                <span class="invalid-feedback"><?php echo $fullname_err; ?></span>
+            </div>     
         </div>        	
         
         <div class="form-group">
-        	<input type="email" class="form-control" name="email" placeholder="Email" required="required">
+        	<input type="email" class="form-control" name="email" placeholder="Email"<?php echo (!empty($email_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $email; ?>">
+                <span class="invalid-feedback"><?php echo $email_err; ?></span>
+            </div>     
         </div>
 		<div class="form-group">
-            <input type="password" class="form-control" name="password" placeholder="Password" required="required">
+            <input type="password" class="form-control" name="password" placeholder="Password" <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>">
+                <span class="invalid-feedback"><?php echo $password_err; ?></span>
+            </div>    
         </div>
 		<div class="form-group">
-            <input type="password" class="form-control" name="confirm_password" placeholder="Confirm Password" required="required">
+            <input type="password" class="form-control" name="confirm_password" placeholder="Confirm Password"<?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_password; ?>">
+                <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
+            </div>     
         </div>
                 
         <div class="form-group">
