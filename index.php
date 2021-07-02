16dd57c0620b7cc3032cd8363e7d2a16d8257f1d
CatFishtech <80201866+CatFishtech@users.noreply.github.com>
2021-07-02 13:11:45 +0300

    home page update

--- index.php
+++ index.php
@@ -31,20 +31,42 @@
   <h1 align="center" >CatFish Tech</h1>
   <h2 align="center">FOOD ORDERING PLATFORM</h2>
 </div>
+</div><br>
+<p style="color: black;font: 1em Times New Roman;text-align: center;"><b>PRICING</b></p>
+<div class="columns">
+  <ul class="price">
+    <li class="header" style="background-color: rgb(20, 214, 240);color: white;"> Premium Package</li>
+    <li class="grey">KSH. 150,000.</li>
+    <li>Gadget Installation @ksh.20,000.</li>
+    <li>Tablets @ksh.80,000. </li>
+    <li>Maintenance @ksh.5,000.</li>
+    <li>Full Platform Access @ksh.30.000.</li>
+    <li><a href="mailto:qimate@hotmail.com" style="background-color: turquoise; padding: 10px 25px;text-align: center;color: white;">Select Package</a></li>
+  </ul>
 </div>
 
- 
+<div class="columns">
+  <ul class="price">
+    <li class="header" style="background-color: rgb(57, 132, 231);color: white;">Basic Package</li>
+    <li class="grey">KSH. 45,000</li>
+    <li>Gadget Installation @ksh.10,000</li>
+    <li>Tablets (user purchased) </li>
+    <li>Maintenance @ksh.5,000</li>
+    <li>Full Platform Access @ksh.30.000</li>
+    <li><a href="mailto:qimate@hotmail.com" style="background-color: turquoise; padding: 10px 25px;text-align: center;color: white;">Select Package</a></li>
+  </ul>
+</div><br>
 
-  <footer class="footer">
-   <h3>CatFishTech<span>Co.</span></h3>
- <div class="mail">
-      <p><a href="mailto:qimate@hotmail.com">support@catfishtech.com</a></p>
-
-      <p>CatFishTechCo. © 2021</p>
-    </div>
-  </footer>
+  
 
 </body>
+<footer style="background-color: black;text-align: center;color: white; padding: 60px; height: 300px;">
+  <h3>CatFishTech<span>Co.</span></h3>
+<div style="text-align: center;color: white;">
+     <p><a href="mailto:qimate@hotmail.com">support@catfishtech.com</a></p>
 
+     <p>CatFishTechCo. © 2021</p>
+   </div>
+ </footer>
 
 </html>
