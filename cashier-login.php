7764df7aeb922a5c9400f232eb77001d5f6a3501
CatFishtech <80201866+CatFishtech@users.noreply.github.com>
2021-07-07 15:09:46 +0300

    update

--- cashier-login.php
+++ cashier-login.php
@@ -15,9 +15,9 @@ if (isset($_POST['submit'])) {
 	$email = $_POST['email'];
 	$password = md5($_POST['password']);
 
-	$sql = "SELECT * FROM users WHERE email='$email', agent='$agent', password='$password'";
+	$sql = "SELECT * FROM users WHERE email='$email', agent='$agent' AND password='$password'";
 	$result = mysqli_query($conn, $sql);
-	if ($result->num_rows > 0) {
+	if ($result->num_rows > 1) {
 		$row = mysqli_fetch_assoc($result);
 		$_SESSION['username'] = $row['username'];
 		header("Location: cashier.php");
