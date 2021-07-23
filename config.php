0f569b3c71f83fe9f1fc7fed469c3bb24f9bc701
CatFishtech <80201866+CatFishtech@users.noreply.github.com>
2021-07-23 10:02:36 +0300

    update

--- config.php
+++ config.php
@@ -1,15 +1,16 @@
-
-<?php 
-
-$server = "catfishtech.database.windows.net";
-$user = "catfish";
-$pass = "Christine61";
-$database = "catfish";
-
-$conn = mysqli_connect($server, $user, $pass, $database);
-
-if (!$conn) {
-    die("<script>alert('Connection Failed.')</script>");
+<?php
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:catfishtech.database.windows.net,1433; Database = catfish", "catfish", "Christine61");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
 }
 
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "catfish", "pwd" => "Christine61", "Database" => "catfish", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:catfishtech.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
